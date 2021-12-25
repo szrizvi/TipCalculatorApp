e3cb0dd9cb3f8ef55f0dfc689fcb353f4d8fe8ad
szrizvi <77036574+szrizvi@users.noreply.github.com>
2021-12-25 19:47:18 +0400

    initial commit

---
+++ Prework/SettingsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SettingsViewController.swift
+//  Prework
+//
+//  Created by Zainab Rizvi on 17/12/2021.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var tip1TextField: UITextField!
+    @IBOutlet weak var tip2TextField: UITextField!
+    @IBOutlet weak var tip3TextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var billAmountTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.title = "Tip Calculator"
     }
 
-
+    @IBAction func calculateTip(_ sender: Any) {
+        let bill = Double(billAmountTextField.text!) ?? 0
+        
+        let tipPercentages = [0.15, 0.18, 0.2]
+        let tip = bill *
+            tipPercentages[tipControl.selectedSegmentIndex]
+        
+        let total = bill + tip
+        
+        tipAmountLabel.text = String(format: "$%.2f", total)
+        totalLabel.text = String(format: "$%.2f", tip)
+    }
+    
 }
 
